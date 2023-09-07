c51d55934233594321f0117cc7bdb67c2363edd0
Darshan Narkhede <115430418+cs-darshan@users.noreply.github.com>
2023-09-07 20:21:05 +0530

    updated

--- main.cpp
+++ main.cpp
@@ -1,122 +1,132 @@
 #include <iostream>
-#include <numeric>
 #include <string>
 #include <sstream>
+#include <boost/multiprecision/cpp_int.hpp>
 
-class encryption
+namespace mp = boost::multiprecision;
+
+class Encryption
 {
 public:
-
-    int p, q, e{}, d{}, T, N;
-    int public_key[2] {};
-    int private_key[2]{};
-    int encryption_value;
-
+    mp::cpp_int p, q, e{}, d{}, T, N;
+    mp::cpp_int public_key[2]{};
+    mp::cpp_int private_key[2]{};
+    mp::cpp_int encryption_value;
 
 public:
-    encryption(int prime1, int prime2)
-    {
-        p = prime1;
-        q = prime2;
-        T =(p-1)*(q-1);
-        N = p * q;
-        encryption_value = 0;
-    }
-
-    encryption() = default;
+    Encryption(const mp::cpp_int& prime1, const mp::cpp_int& prime2)
+        : p(prime1), q(prime2), T((p - 1) * (q - 1)), N(p * q), encryption_value(0) {}
 
 public:
-    int prime_check() const{
-        int ctr = 0;
-        for (int i = 1; i < p; i++) {
-            if (i % p == 0) {
-                std::cout << "not a prime number" << std::endl;
-                ctr++;
-            }
-        }
-        for (int j = 1; j < q; j++) {
-            if (j % q == 0) {
-                std::cout << "not a prime number" << std::endl;
-                ctr++;
-            }
+    bool prime_check(const mp::cpp_int& num) const {
+        if (num <= 1)
+            return false;
 
+        for (mp::cpp_int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                std::cout << "Not a prime number" << std::endl;
+                return false;
+            }
         }
         return true;
     }
 
-    void key()
-    {
-        for(e = 2; e<T; e++)
-        {
-            if(T%e==1 || N%e==1) {
-                public_key[0]=e;
-            }
-        }
-        for(d = 1; d>0; d++)
-        {
-            long long value = public_key[0]*d % T;
-            if(value== 1 && d!=public_key[0])
-            {
-                private_key[0]=d;
+    void key() {
+        for (e = 2; e < T; ++e) {
+            if (mp::gcd(e, T) == 1) {
+                public_key[0] = e;
                 break;
             }
         }
+
+        d = mp::powm(public_key[0], -1, T);
+
         public_key[1] = N;
+        private_key[0] = d;
         private_key[1] = N;
 
         std::cout << "public_key[e] = " << public_key[0] << std::endl;
-        std::cout << "public_key[N] = " << public_key[1] <<std::endl;
+        std::cout << "public_key[N] = " << public_key[1] << std::endl;
         std::cout << "private_key[d] = " << private_key[0] << std::endl;
         std::cout << "private_key[N] = " << private_key[1] << std::endl;
     }
 
-    int character_encryption(int m)
-    {
-        int temp = m;
-        encryption_value = 1;
-        for (int i = 1; i<=public_key[0]; i++)
-        {
-            m = temp*m;
-        }
-        encryption_value = m % public_key[1];
+    mp::cpp_int character_encryption(const mp::cpp_int& m) {
+        encryption_value = mp::powm(m, public_key[0], public_key[1]);
         return encryption_value;
     }
+};
+
+class Decryption
+{
 public:
-    ~encryption()= default;
+    mp::cpp_int d, N;
+    mp::cpp_int decryption_value;
+
+public:
+    Decryption(const mp::cpp_int& private_key_d, const mp::cpp_int& public_key_N)
+        : d(private_key_d), N(public_key_N), decryption_value(0) {}
+
+    mp::cpp_int character_decryption(const mp::cpp_int& c) {
+        decryption_value = mp::powm(c, d, N);
+        return decryption_value;
+    }
 };
 
 int main() {
-
     char ch;
-    std::cout << "enter e for encryption"<<std::endl;
-    std::cin>>ch;
+    std::cout << "Enter 'e' for encryption or 'd' for decryption" << std::endl;
+    std::cin >> ch;
 
-    if(ch == 'e') {
-        int p;
-        int q;
-        std::cout << "enter 2 prime numbers for p and q" << std::endl;
+    if (ch == 'e') {
+        mp::cpp_int p, q;
+        std::cout << "Enter two prime numbers for p and q" << std::endl;
         std::cin >> p >> q;
-        encryption encrypt(p, q);
-        encrypt.prime_check();
+        Encryption encrypt(p, q);
 
-        std::string statement;
-        std::cout << "enter statement" << std::endl;
+        if (!encrypt.prime_check(p) || !encrypt.prime_check(q)) {
+            std::cout << "Invalid prime numbers" << std::endl;
+            return 1;
+        }
+
         std::cin.ignore();
-        getline(std::cin, statement);
+        std::string statement;
+        std::cout << "Enter a statement" << std::endl;
+        std::getline(std::cin, statement);
 
-
-        char ch_statement;
         encrypt.key();
-        int temp;
-        int encryption_e;
-        for (int i = 0; statement[i]; i++) {
-            ch_statement = static_cast<int>(statement[i]);
-            encryption_e = encrypt.character_encryption(ch_statement);
-            temp = ch_statement + encryption_e;
+
+        std::cout << "Encrypted statement: ";
+        for (char ch : statement) {
+            mp::cpp_int ch_statement = static_cast<mp::cpp_int>(ch);
+            mp::cpp_int encryption_e = encrypt.character_encryption(ch_statement);
+            mp::cpp_int temp = ch_statement + encryption_e;
             std::cout << temp << " ";
         }
-        std::cout << std::endl << std::endl;
-        encrypt.~encryption();
+        std::cout << std::endl;
+    } else if (ch == 'd') {
+        mp::cpp_int private_key_d, public_key_N;
+        std::cout << "Enter private key (d) and public key (N) for decryption" << std::endl;
+        std::cin >> private_key_d >> public_key_N;
+
+        Decryption decrypt(private_key_d, public_key_N);
+
+        std::string encrypted_statement;
+        std::cout << "Enter the encrypted statement to decrypt" << std::endl;
+        std::cin.ignore();
+        std::getline(std::cin, encrypted_statement);
+
+        std::cout << "Decrypted statement: ";
+        std::istringstream iss(encrypted_statement);
+        mp::cpp_int c;
+        while (iss >> c) {
+            mp::cpp_int decrypted_char = decrypt.character_decryption(c);
+            char original_char = static_cast<char>(decrypted_char);
+            std::cout << original_char;
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "Invalid choice" << std::endl;
     }
 
     return 0;
